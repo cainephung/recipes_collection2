859cf7186b6d872f98e8f207c97d04c14bc36e40
Caine Phung <Hphung3324@conestogac.on.ca>
2023-02-23 00:36:26 -0500

    add list.js

--- collection/src/pages/index.js
+++ collection/src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +15,9 @@ export default function Home() {
         <div className='text-center justify-center '>
           <div>
             <p className='font-black'> CAINE'S COLLECTION</p>
-            <p className=' text-sm font-bold md:text-xl p-4'> This is my personal recipes collection of simple or simplify version dishes from various sources, that are suitable for everyone, but especially for people who want to cook something relatively easy but stil decicious!</p>
-            <button className='cursor-pointer px-5 py-1'>Let's Begin!</button>        
+            <p className=' text-sm font-bold md:text-xl p-10'> This is my personal recipes collection of simple or simplify version dishes from various sources.<br/>Suitable for everyone, but especially for people who want to cook something relatively easy but stil decicious!</p>
+            <Link href='/list'><button className='cursor-pointer px-5 py-1'>Let's Begin!</button>        
+            </Link>
           </div>
 
           
---
+++ collection/src/pages/list.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import Link from "next/link";
+import list from "../pages/list.js"
+
+const inter = Inter({ subsets: ['latin'] })
+
+const List =() =>{
+    return (
+        <div className=' w-full font-black pt-16'>
+            
+          </div>
+    )
+}
+export default List
