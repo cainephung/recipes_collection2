9afa04e60c9df0fd16a30d400487a7ebe7adec79
Caine Phung <Hphung3324@conestogac.on.ca>
2023-02-23 21:07:48 -0500

    setting up list and adding pics

--- collection/src/pages/index.js
+++ collection/src/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Link from "next/link";
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,7 +17,7 @@ export default function Home() {
           <div>
             <p className='font-black'> CAINE'S COLLECTION</p>
             <p className=' text-sm font-bold md:text-xl p-10'> This is my personal recipes collection of simple or simplify version dishes from various sources.<br/>Suitable for everyone, but especially for people who want to cook something relatively easy but stil decicious!</p>
-            <Link href='/list'><button className='cursor-pointer px-5 py-1'>Let's Begin!</button>        
+            <Link href='/'><button className='cursor-pointer px-5 py-1'>Let's Begin!</button>        
             </Link>
           </div>
 
--- collection/src/pages/list.js
+++ collection/src/pages/list.js
@@ -2,15 +2,134 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Link from "next/link";
-import list from "../pages/list.js"
+import lm from '../pics/loco_moco.png'
+import kb from '../pics/kb.png'
+import tk from '../pics/tk.png'
+import hns from '../pics/hns.png'
+import bs from '../pics/bs.png'
+
+
+
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-const List =() =>{
+const List =() => {
+
     return (
-        <div className=' w-full font-black pt-16'>
+    <div id='Skills' className='w-full pt-16'>
+    <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-3xl text-center tracking-widest uppercase text-[#5651e5] font-black'>Recipes</p>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-20 pt-10'>
+
+        <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+        <div className='grid grid-col-2 gap-4 justify-center items-center'>
             
-          </div>
-    )
+            <div className='flex flex-col items-center justify-center font-bold'>
+                <h3 className='text-sm'>Loco Moco</h3>
+            </div>
+
+            <div className='m-auto'>
+                <Image  src={lm}  width={500} height={500} alt='/' ></Image> 
+            </div>
+
+            <div className='flex flex-col items-center justify-center font-black'>
+                <Link href='/list'>
+                    <button className='py-2 px-4'>More</button>
+                </Link>
+            </div>
+        </div>
+        </div>
+
+
+        
+        <div className='p-6 shadow-2xl rounded-xl hover:scale-105 ease-in duration-300'>
+        <div className='grid grid-col-2 gap-4 justify-center items-center'>
+            
+            <div className='flex flex-col items-center justify-center font-bold'>
+                <h3 className='text-sm'>Korean Beef</h3>
+            </div>
+
+            <div className='m-auto'>
+                <Image  src={kb}  width={500} height={500} alt='/' ></Image> 
+            </div>
+
+            <div className='flex flex-col items-center justify-center font-black'>
+                <Link href='/list'>
+                    <button className='py-2 px-4'>More</button>
+                </Link>
+            </div>
+        </div>
+        </div>
+
+
+        <div className='p-6 shadow-2xl rounded-xl hover:scale-105 ease-in duration-300'>
+        <div className='grid grid-col-2 gap-4 justify-center items-center'>
+            
+            <div className='flex flex-col items-center justify-center font-bold'>
+                <h3 className='text-sm'>Tom Kha Gai</h3>
+            </div>
+
+            <div className='m-auto'>
+                <Image  src={tk}  width={500} height={500} alt='/' ></Image> 
+            </div>
+
+            <div className='flex flex-col items-center justify-center font-black'>
+                <Link href='/list'>
+                    <button className='py-2 px-4'>More</button>
+                </Link>
+            </div>
+        </div>
+        </div>
+        
+
+        <div className='p-6 shadow-2xl rounded-xl hover:scale-105 ease-in duration-300'>
+        <div className='grid grid-col-2 gap-4 justify-center items-center'>
+            
+            <div className='flex flex-col items-center justify-center font-bold'>
+                <h3 className='text-sm'>Hot & Sour Soup</h3>
+            </div>
+
+            <div className='m-auto'>
+                <Image  src={hns}  width={500} height={500} alt='/' ></Image> 
+            </div>
+
+            <div className='flex flex-col items-center justify-center font-black'>
+                <Link href='/list'>
+                    <button className='py-2 px-4'>More</button>
+                </Link>
+            </div>
+        </div>
+        </div>
+
+
+        <div className='p-6 shadow-2xl rounded-xl hover:scale-105 ease-in duration-300'>
+        <div className='grid grid-col-2 gap-4 justify-center items-center'>
+            
+            <div className='flex flex-col items-center justify-center font-bold'>
+                <h3 className='text-sm'>Beef Stew</h3>
+            </div>
+
+            <div className='m-auto'>
+                <Image  src={bs}  width={500} height={500} alt='/' ></Image> 
+            </div>
+
+            <div className='flex flex-col items-center justify-center font-black'>
+                <Link href='/list'>
+                    <button className='py-2 px-4'>More</button>
+                </Link>
+            </div>
+        </div>
+        </div>
+        
+
+       
+
+        </div>
+    </div>
+</div>
+)
 }
+
 export default List
